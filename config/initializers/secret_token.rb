7ccfd3c860e6625212947d7a5754b032387e4a63
wusher <willusher+github@gmail.com>
2010-12-06 17:57:40 -0800

    created the rails proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TopicVoter::Application.config.secret_token = '8e574a30c7099ebc8d960e04110bfca155a3327f829c8ddaef0b83548c14b298fc51b6041ff38c6c10b852a717451b3a2aafd55b9b4e2548e00a2c9f5f8cc93d'
